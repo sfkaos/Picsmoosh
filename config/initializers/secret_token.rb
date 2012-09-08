26d6154d68f8762442b9175e64529b8f2322b6de
Ajay Varia <ajayvaria@piazza-devel.local>
2012-09-08 15:17:19 -0700

    initial branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hack::Application.config.secret_token = '3ed57367e0a784e80c50063095c0e9ac8af58a6fd2a9e2ce5bbb93d96f2a28de9e9abfa451a299031909f203aec554d66d18c074dfcb3648511c0eb3927c949f'
